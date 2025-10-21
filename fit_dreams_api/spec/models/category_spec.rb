ee0371e2429076533184b959e9a7897b9a349d9a
Gabriel <211056000@aluno.unb.br>
2025-10-21 09:39:47 -0300

    feat(models): Phase 2 - Category presence validation and tests; docs for tests

--- fit_dreams_api/spec/models/category_spec.rb
+++ fit_dreams_api/spec/models/category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires name' do
+    c = Category.new
+    expect(c.valid?).to be_falsey
+    expect(c.errors[:name]).to be_present
+  end
+
+  it 'is valid with a name' do
+    c = Category.new(name: 'Treino')
+    expect(c.valid?).to be_truthy
+  end
 end
